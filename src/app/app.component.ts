7b9ddf22ae27294e92a3e8228019d688b415c924
Klea <kleaprifti21@gmail.com>
2023-10-18 11:51:33 +0200

    environment changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/production-environment/environment.production';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: '<p>API URL: {{ apiUrl }}</p>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'parking-system';
+  apiUrl = environment.apiUrl;
 }
