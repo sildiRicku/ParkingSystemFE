b578e7034758bc963d408f732a35f88a989a91f9
Klea <kleaprifti21@gmail.com>
2023-12-15 14:58:32 +0100

    ..

--- src/app/auth.guard.spec.ts
+++ src/app/auth.guard.spec.ts
@@ -1,17 +1,68 @@
-import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { SessionService } from './session.service';
+import { AuthServiceService } from './auth.service';
 
-import { authGuard } from './auth.guard';
+class MockAuthService {
+  isAuthenticated(): boolean {
+    return true;
+  }
+}
 
-describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+class MockSessionService {
+}
+
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
+  let router: Router;
+  let authService: MockAuthService;
+  let sessionService: MockSessionService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [
+        AuthGuard,
+        { provide: AuthServiceService, useClass: MockAuthService },
+        { provide: SessionService, useClass: MockSessionService },
+      ],
+    });
+  }));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    guard = TestBed.inject(AuthGuard);
+    router = TestBed.inject(Router);
+    authService = TestBed.inject(AuthServiceService) as MockAuthService;
+    sessionService = TestBed.inject(SessionService) as MockSessionService;
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(guard).toBeTruthy();
+  });
+
+  it('should allow activation when authenticated', () => {
+    spyOn(authService, 'isAuthenticated').and.returnValue(true);
+
+    const route: ActivatedRouteSnapshot = {} as ActivatedRouteSnapshot;
+    const state: RouterStateSnapshot = {} as RouterStateSnapshot;
+
+    const canActivate = guard.canActivate(route, state);
+
+    expect(canActivate).toEqual(true);
+  });
+
+  it('should navigate to "/" when not authenticated', () => {
+    spyOn(authService, 'isAuthenticated').and.returnValue(false);
+    spyOn(router, 'navigate');
+
+    const route: ActivatedRouteSnapshot = {} as ActivatedRouteSnapshot;
+    const state: RouterStateSnapshot = {} as RouterStateSnapshot;
+
+    const canActivate = guard.canActivate(route, state);
+
+    expect(canActivate).toEqual(false);
+    expect(router.navigate).toHaveBeenCalledWith(['/']);
   });
 });
