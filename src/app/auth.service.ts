3b4bfeb3b222f60859a198db75633e4beef0e310
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-12-29 17:33:03 +0100

    update auth

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/development-environment/environment';
 import { SessionService } from './session.service';
 
@@ -12,39 +13,55 @@ export class AuthServiceService {
   private baseUrl = environment.apiUrl;
   private isLoggedIn = false;
 
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private sessionService: SessionService
+  ) {}
 
-  constructor(private http: HttpClient,private router:Router,private sessionService: SessionService) { }
-  login(username: string, password: string): Observable<any> {
-
+  login(username: string, password: string, rememberMe: boolean = false): Observable<any> {
     this.isLoggedIn = true;
-    setTimeout(() => {
-      this.logout();
-    }, 30000);
 
-    const loginData = { username,password };
-    console.log('Request payload:', loginData); 
+    if (!rememberMe) {
+      setTimeout(() => {
+        this.logout();
+      }, 30000);
+    }
 
-    const headers = new HttpHeaders({
+    const loginData = { username, password };
+    console.log('Request payload:', loginData);
+
+    const headers = this.createHeaders(loginData);
+
+    const apiUrl = `${this.baseUrl}/login`;
+    
+    return this.http.post(apiUrl, loginData, { headers, responseType: 'text' }).pipe(
+      tap(() => {
+        if (rememberMe) {
+          this.sessionService.setUsername(username);
+        }
+      }),
+      catchError((error) => {
+        console.error('Login error:', error);
+        return throwError(error);
+      })
+    );
+  }
+
+  private createHeaders(loginData: { username: string; password: string }): HttpHeaders {
+    return new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': 'Basic ' + btoa(`${loginData.username}:${loginData.password}`)
-  });
-
-
-  return this.http.post(`${this.baseUrl}/login`, loginData, { headers, responseType: 'text' }).pipe(
-    tap(() => {
-      this.sessionService.setUsername(username);
-    })
-  );
-}
-
-
-logout() {
-  this.isLoggedIn = false;
-  this.sessionService.clearUsername(); 
-  this.router.navigate(['/']);
-}
-
-isAuthenticated() {
-  return this.isLoggedIn;
-}
+    });
   }
+
+  logout() {
+    this.isLoggedIn = false;
+    this.sessionService.clearUsername();
+    this.router.navigate(['/']);
+  }
+
+  isAuthenticated() {
+    return this.isLoggedIn;
+  }
+}
